4e227716761265820eba0a786f7c5ee7eaec7b9c
md-shawket <101445128+md-shawket@users.noreply.github.com>
2023-04-27 06:10:46 +0000

    Data Rendered from List

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+all_jobs = [{
+  'id': 1,
+  'post': 'Data Analysis',
+  'salary': 1000,
+  'location': 'HQ'
+}, {
+  'id': 2,
+  'post': 'Data Scientist',
+  'salary': 1100,
+  'location': 'Mirpur'
+}, {
+  'id': 3,
+  'post': 'Frontend Eng',
+  'salary': 1300,
+  'location': 'Dhaka'
+}, {
+  'id': 4,
+  'post': 'Backend Eng',
+  'salary': 1400,
+  'location': 'Khula'
+}]
 
 
 @app.route("/")
 def hello_world():
-  return '<p> My name is Shawket Ali <p>'
+  return render_template('home.html', jobs=all_jobs)
 
 
 if __name__ == '__main__':
